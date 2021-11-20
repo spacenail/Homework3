97ce6783b5874851f9e1a59c944fae95e6739929
Artem Meshcherov <spacenail@gmail.com>
2021-11-20 14:33:40 +0300

    done

---
+++ src/Box.java
@@ -0,0 +1,58 @@
+/*
+Задача:
+1.Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
+2.Класс Box, в который можно складывать фрукты. Коробки условно сортируются по типу фрукта,
+ поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
+3.Для хранения фруктов внутри коробки можно использовать ArrayList;
+4.Сделать метод getWeight(), который высчитывает вес коробки, зная вес одного фрукта
+ и их количество: вес яблока – 1.0f, апельсина – 1.5f (единицы измерения не важны);
+5.Внутри класса Box сделать метод compare(), который позволяет сравнить текущую коробку
+ с той, которую подадут в compare() в качестве параметра. true – если их массы равны,
+  false в противоположном случае. Можно сравнивать коробки с яблоками и апельсинами;
+6.Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую.
+ Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
+  Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты,
+  которые были в первой;
+Не забываем про метод добавления фрукта в коробку.
+ */
+
+import java.util.ArrayList;
+
+public class Box<I extends Fruit> {
+    private ArrayList<I> list;
+
+    public Box() {
+        list = new ArrayList<>();
+    }
+
+    public void add(I item){
+        list.add(item);
+    }
+
+    public float getWeight(){
+        return list.stream().
+                reduce(0f,
+                        (x,y)-> x + y.getWeight(),
+                        (x,y) -> x+y);
+    }
+    /*
+    public float getWeight(){
+        float sum = 0f;
+        for(I item:list){
+            sum+= item.getWeight();
+        }
+        return sum;
+    }
+    */
+
+    public boolean compare(Box<?> anotherBox){
+        return this.getWeight() > anotherBox.getWeight();
+    }
+
+    public void pour(Box<I> anotherBox){
+        for (I item:list){
+            anotherBox.add(item);
+        }
+        list.clear();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        String[] stringArray = {"one","two"};
+        Integer[] intArray = {1,2};
+
+        Arrays.stream(stringArray).forEach(s -> System.out.print(s + " "));
+        System.out.println();
+        swap(stringArray,0,1);
+        Arrays.stream(stringArray).forEach(s -> System.out.print(s + " "));
+        System.out.println();
+
+        Arrays.stream(intArray).forEach(s -> System.out.print(s + " "));
+        System.out.println();
+        swap(intArray,0,1);
+        Arrays.stream(intArray).forEach(s -> System.out.print(s + " "));
+        System.out.println();
+
+        ArrayList newList = toArrayList(stringArray);
+        System.out.println(newList.get(0));
+
+        Box<Apple> appleBox = new Box<>();
+        appleBox.add(new Apple());
+        System.out.println(appleBox.getWeight());
+
+        Box<Apple> anotherAppleBox = new Box<>();
+        anotherAppleBox.add(new Apple());
+        anotherAppleBox.pour(appleBox);
+        System.out.println(appleBox.getWeight());
+
+        Box<Orange> orangeBox = new Box<>();
+        orangeBox.add(new Orange());
+        System.out.println(orangeBox.getWeight());
+        System.out.println(appleBox.compare(orangeBox));
+
+    }
+
+
+    /*
+    Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+     */
+    public static <T> void swap(T[] array,int ind1,int ind2){
+        T temp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = temp;
+    }
+
+    /*
+    Написать метод, который преобразует массив в ArrayList;
+     */
+    public static <T> ArrayList<T> toArrayList(T[] array){
+        return new ArrayList(Arrays.asList(array));
     }
 }
