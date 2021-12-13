1634a192db7c04fe0ae84b125e8c5fdbd321f92f
Artem Meshcherov <spacenail@gmail.com>
2021-12-13 20:58:28 +0300

    add brif

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,18 @@
 public class Main {
-    private volatile char expectedLetter = 'A';
-    private Object monitor = new Object();
     public static void main(String[] args) {
         /*
-        1. Создать три потока, каждый из которых выводит
-         определенную букву (A, B и C) 5 раз (порядок – ABСABСABС).
-          Используйте wait/notify/notifyAll.
+        1. Добавить на серверную сторону чата логирование, с выводом информации о
+         действиях на сервере (запущен, произошла ошибка, клиент подключился, клиент
+         прислал сообщение/команду).
+2. Написать метод, которому в качестве аргумента передается не пустой одномерный целочисленный
+ массив. Метод должен вернуть новый массив, который получен путем вытаскивания из исходного
+ массива элементов, идущих после последней четверки. Входной массив должен содержать
+ хотя бы одну четверку, иначе в методе необходимо выбросить RuntimeException.
+ Написать набор тестов для этого метода (по 3-4 варианта входных данных).
+ Вх: [ 1 2 4 4 2 3 4 1 7 ] -> вых: [ 1 7 ].
+3. Написать метод, который проверяет состав массива из чисел 1 и 4. Если в нем нет хоть
+одной четверки или единицы, то метод вернет false; Написать набор тестов для этого метода
+(по 3-4 варианта входных данных).
          */
-        Main main = new Main();
-
-        new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                main.printA();
-            }
-        }).start();
-
-        new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                main.printB();
-            }
-        }).start();
-
-        new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                main.printC();
-            }
-        }).start();
-    }
-
-    public void printA(){
-        synchronized (monitor){
-            while (expectedLetter != 'A'){
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.print(expectedLetter);
-            expectedLetter = 'B';
-            monitor.notifyAll();
-        }
-    }
-
-    public void printB(){
-        synchronized (monitor){
-            while (expectedLetter != 'B'){
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.print(expectedLetter);
-            expectedLetter = 'C';
-            monitor.notifyAll();
-        }
-    }
-
-    public void printC(){
-        synchronized (monitor){
-            while (expectedLetter != 'C'){
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.print(expectedLetter);
-            expectedLetter = 'A';
-            monitor.notifyAll();
-        }
     }
 }
